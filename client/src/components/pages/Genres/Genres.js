e7d303787291cfb2725534aca042c8bdbf76a375
Aram Arakelyan <aram777arakelyan@gmail.com>
2024-03-16 16:15:38 +0400

    added genres grid

--- client/src/components/pages/Genres/Genres.js
+++ client/src/components/pages/Genres/Genres.js
@@ -1,41 +1,20 @@
 import React from "react"
 import "../../../assets/texts.css"
+import { IMAGES } from "./images"
+import "./Genres.css"
 
 const Genres = () => {
     return (
-        <div>
+        <div className="genres">
             <h1 className="bigger-heading">GENRES</h1>
-            <div class="row">
-                <div class="column">
-                    <img src="" alt="genre" />
-                    <img src="" alt="genre" />
-                    <img src="" alt="genre" />
-                    <img src="" alt="genre" />
-                </div>
-                <div class="column">
-                    <img src="" alt="genre" />
-                    <img src="" alt="genre" />
-                    <img src="" alt="genre" />
-                    <img src="" alt="genre" />
-                </div>
-                <div class="column">
-                    <img src="" alt="genre" />
-                    <img src="" alt="genre" />
-                    <img src="" alt="genre" />
-                    <img src="" alt="genre" />
-                </div>
-                <div class="column">
-                    <img src="" alt="genre" />
-                    <img src="" alt="genre" />
-                    <img src="" alt="genre" />
-                    <img src="" alt="genre" />
-                </div>
-                <div class="column">
-                    <img src="" alt="genre" />
-                    <img src="" alt="genre" />
-                    <img src="" alt="genre" />
-                    <img src="" alt="genre" />
-                </div>
+            <div className="row">
+                {IMAGES.map((imageGroup) => (
+                    <div key={imageGroup[0].id} className="column">
+                        {imageGroup.map((image) => (
+                            <img key={image.id} src={image.src} alt="genre" />
+                        ))}
+                    </div>
+                ))}
             </div>
         </div>
     )
---
+++ client/src/components/pages/Genres/images.js
@@ -0,0 +1,86 @@
+import image1 from "../../../assets/images/genres/Action.png"
+import image2 from "../../../assets/images/genres/ADVENTURE.png"
+import image3 from "../../../assets/images/genres/Comedy.png"
+import image4 from "../../../assets/images/genres/Detective.png"
+import image5 from "../../../assets/images/genres/Drama.png"
+import image6 from "../../../assets/images/genres/Fantasy.png"
+import image7 from "../../../assets/images/genres/FIGHTING.png"
+import image8 from "../../../assets/images/genres/Horror.png"
+import image9 from "../../../assets/images/genres/Manga.png"
+import image10 from "../../../assets/images/genres/MUSIC.png"
+import image11 from "../../../assets/images/genres/MYSTERY.png"
+import image12 from "../../../assets/images/genres/SCI-FI.png"
+import image13 from "../../../assets/images/genres/SLASHER.png"
+import image14 from "../../../assets/images/genres/Thriller.png"
+import image15 from "../../../assets/images/genres/WESTERN.png"
+
+export const IMAGES = [
+    [
+        {
+            id: 1,
+            src: image1,
+        },
+        {
+            id: 2,
+            src: image2,
+        },
+        {
+            id: 3,
+            src: image3,
+        },
+        {
+            id: 4,
+            src: image4,
+        },
+    ],
+    [
+        {
+            id: 5,
+            src: image5,
+        },
+        {
+            id: 6,
+            src: image6,
+        },
+        {
+            id: 7,
+            src: image7,
+        },
+        {
+            id: 8,
+            src: image8,
+        },
+    ],
+    [
+        {
+            id: 9,
+            src: image9,
+        },
+        {
+            id: 10,
+            src: image10,
+        },
+        {
+            id: 11,
+            src: image11,
+        },
+        {
+            id: 12,
+            src: image12,
+        },
+    ],
+    [
+        {
+            id: 13,
+            src: image13,
+        },
+        {
+            id: 14,
+            src: image14,
+        },
+        {
+            id: 15,
+            src: image15,
+        },
+    ],
+]
