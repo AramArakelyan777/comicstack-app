84f4154adfcb9cae730e88b115b8f335e8de27c0
Aram Arakelyan <aram777arakelyan@gmail.com>
2024-03-18 17:53:55 +0400

    added eslintignore, minor changes

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -5,41 +5,43 @@ import "../../assets/texts.css"
 
 const Navbar = () => {
     return (
-        <nav className="navbar">
-            <NavLink to="/genres">
-                <div className="navbarLink">
-                    <MenuIcon name="genres" />
-                    <span className="menu-text">GENRES</span>
-                </div>
-            </NavLink>
+        <div className="navbar-container">
+            <nav className="navbar">
+                <NavLink to="/genres">
+                    <div className="navbarLink">
+                        <MenuIcon name="genres" />
+                        <span className="menu-text">GENRES</span>
+                    </div>
+                </NavLink>
 
-            <NavLink to="/faq">
-                <div className="navbarLink">
-                    <MenuIcon name="faq" />
-                    <span className="menu-text">FAQ</span>
-                </div>
-            </NavLink>
+                <NavLink to="/faq">
+                    <div className="navbarLink">
+                        <MenuIcon name="faq" />
+                        <span className="menu-text">FAQ</span>
+                    </div>
+                </NavLink>
 
-            <NavLink to="/">
-                <div className="homeNavLink">
-                    <MenuIcon name="home" />
-                </div>
-            </NavLink>
+                <NavLink to="/">
+                    <div className="homeNavLink">
+                        <MenuIcon name="home" />
+                    </div>
+                </NavLink>
 
-            <NavLink to="/forum">
-                <div className="navbarLink">
-                    <MenuIcon name="forum" />
-                    <span className="menu-text">FORUM</span>
-                </div>
-            </NavLink>
+                <NavLink to="/forum">
+                    <div className="navbarLink">
+                        <MenuIcon name="forum" />
+                        <span className="menu-text">FORUM</span>
+                    </div>
+                </NavLink>
 
-            <NavLink to="/login">
-                <div className="navbarLink">
-                    <MenuIcon name="user" />
-                    <span className="menu-text">USER</span>
-                </div>
-            </NavLink>
-        </nav>
+                <NavLink to="/login">
+                    <div className="navbarLink">
+                        <MenuIcon name="user" />
+                        <span className="menu-text">USER</span>
+                    </div>
+                </NavLink>
+            </nav>
+        </div>
     )
 }
 
--- client/src/components/pages/FAQ/FAQ.js
+++ client/src/components/pages/FAQ/FAQ.js
@@ -1,4 +1,12 @@
-import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Box, ChakraProvider } from "@chakra-ui/react"
+import {
+    Accordion,
+    AccordionButton,
+    AccordionIcon,
+    AccordionItem,
+    AccordionPanel,
+    Box,
+    ChakraProvider,
+} from "@chakra-ui/react"
 import React from "react"
 import "../../../assets/texts.css"
 import "./FAQ.css"
@@ -7,10 +15,12 @@ const FAQ = () => {
     return (
         <ChakraProvider>
             <div className="FAQ">
-                <h1 className="bigger-heading faq-heading">FAQ</h1>
-                <p id="accent">Looking for answers? Here you can find them to the most frequently asked questions.</p>
+                <h1 className="bigger-heading">FAQ</h1>
+                <p className="medium-heading">
+                    Looking for answers? Here you can find them to the most
+                    frequently asked questions.
+                </p>
                 <Accordion allowToggle className="accordion">
-
                     <AccordionItem className="accordion-item">
                         <h2>
                             <AccordionButton
@@ -23,7 +33,25 @@ const FAQ = () => {
                             </AccordionButton>
                         </h2>
                         <AccordionPanel textAlign="left">
-                            Welcome to <code>ComicStack</code>, your portal to the captivating world of comics! Founded in 2023 and headquartered in the picturesque city of Yerevan, Armenia, <code>ComicStack</code> is a passionate and dynamic team of creators, artists, and storytellers dedicated to bringing the magic of comics to life. Our journey began with a shared love for the art of storytelling through visuals and words. We embarked on a mission to craft immersive and thought-provoking narratives that transport readers to new dimensions, and since then, we've never looked back. Behind every comic, there's a team of dedicated individuals who pour their hearts and souls into their work. Our talented artists, writers, editors, and visionaries work in harmony to create stories that resonate with readers of all ages. As we continue to expand our universe of comics, we invite you to embark on this incredible journey with us.
+                            Welcome to <code>ComicStack</code>, your portal to
+                            the captivating world of comics! Founded in 2023 and
+                            headquartered in the picturesque city of Yerevan,
+                            Armenia, <code>ComicStack</code> is a passionate and
+                            dynamic team of creators, artists, and storytellers
+                            dedicated to bringing the magic of comics to life.
+                            Our journey began with a shared love for the art of
+                            storytelling through visuals and words. We embarked
+                            on a mission to craft immersive and
+                            thought-provoking narratives that transport readers
+                            to new dimensions, and since then, we've never
+                            looked back. Behind every comic, there's a team of
+                            dedicated individuals who pour their hearts and
+                            souls into their work. Our talented artists,
+                            writers, editors, and visionaries work in harmony to
+                            create stories that resonate with readers of all
+                            ages. As we continue to expand our universe of
+                            comics, we invite you to embark on this incredible
+                            journey with us.
                         </AccordionPanel>
                     </AccordionItem>
 
@@ -41,9 +69,9 @@ const FAQ = () => {
                         <AccordionPanel textAlign="left">
                             To create an account you need to press User icon on
                             bottom right. After that choose{" "}
-                            <code>Create an account</code> option. Then you need to
-                            enter your email/username(you can use it to log in),
-                            password and password confirmation.
+                            <code>Create an account</code> option. Then you need
+                            to enter your email/username(you can use it to log
+                            in), password and password confirmation.
                         </AccordionPanel>
                     </AccordionItem>
 
@@ -53,13 +81,14 @@ const FAQ = () => {
                                 _expanded={{ bg: "#DB4947", color: "white" }}
                             >
                                 <Box as="span" flex="1" textAlign="left">
-                                    Where can I see all categories?
+                                    Where can I see all genres?
                                 </Box>
                                 <AccordionIcon />
                             </AccordionButton>
                         </h2>
                         <AccordionPanel textAlign="left">
-                            You can find all categories after pressing <code>Genres</code> button on the bottom left.
+                            You can find all categories after pressing{" "}
+                            <code>Genres</code> button on the bottom left.
                         </AccordionPanel>
                     </AccordionItem>
 
@@ -75,9 +104,10 @@ const FAQ = () => {
                             </AccordionButton>
                         </h2>
                         <AccordionPanel textAlign="left">
-                            You need to reach us by <code>info@comicstack.com</code> email. Ensure to tell us your
-                            username or
-                            email so that we can reward you with premium subscription (for 30 days).
+                            You need to reach us by{" "}
+                            <code>info@comicstack.com</code> email. Ensure to
+                            tell us your username or email so that we can reward
+                            you with premium subscription (for 30 days).
                         </AccordionPanel>
                     </AccordionItem>
 
@@ -93,7 +123,8 @@ const FAQ = () => {
                             </AccordionButton>
                         </h2>
                         <AccordionPanel textAlign="left">
-                            When you open a comics description page, scroll to the bottom and Bob's your uncle!
+                            When you open a comics description page, scroll to
+                            the bottom and Bob's your uncle!
                         </AccordionPanel>
                     </AccordionItem>
 
@@ -109,8 +140,9 @@ const FAQ = () => {
                             </AccordionButton>
                         </h2>
                         <AccordionPanel textAlign="left">
-                            In the top right of your comment, you can find three dots. After clicking on it, you can
-                            edit/delete your comment.
+                            In the top right of your comment, you can find three
+                            dots. After clicking on it, you can edit/delete your
+                            comment.
                         </AccordionPanel>
                     </AccordionItem>
 
@@ -126,9 +158,11 @@ const FAQ = () => {
                             </AccordionButton>
                         </h2>
                         <AccordionPanel textAlign="left">
-                            The creators of this wonder are Aram Arakelyan and Davit Tadevosyan. Here is how you can reach
-                            us:<br />
-                            <code>https://github.com/AramArakelyan777</code><br />
+                            The creators of this wonder are Aram Arakelyan and
+                            Davit Tadevosyan. Here is how you can reach us:
+                            <br />
+                            <code>https://github.com/AramArakelyan777</code>
+                            <br />
                             <code>https://github.com/DavitT017</code>
                         </AccordionPanel>
                     </AccordionItem>
@@ -145,7 +179,8 @@ const FAQ = () => {
                             </AccordionButton>
                         </h2>
                         <AccordionPanel textAlign="left">
-                            Yes, you surely can pay with any card including Visa, MasterCard, and even American Express.
+                            Yes, you surely can pay with any card including
+                            Visa, MasterCard, and even American Express.
                         </AccordionPanel>
                     </AccordionItem>
 
@@ -155,17 +190,21 @@ const FAQ = () => {
                                 _expanded={{ bg: "#DB4947", color: "white" }}
                             >
                                 <Box as="span" flex="1" textAlign="left">
-                                    Is it possible to get back my suspended account?
+                                    Is it possible to get back my suspended
+                                    account?
                                 </Box>
                                 <AccordionIcon />
                             </AccordionButton>
                         </h2>
                         <AccordionPanel textAlign="left">
-                            To get back your suspended account, you should write an email to
-                            {" "}<code>support@comicstack.com</code> to explain why we should get back your account and that you
-                            guarantee that you will never violate our <code>Terms of Use</code> again.<br />
-                            Do not forget to write your BAN code in the subject of the email(visit our support page for
-                            details).
+                            To get back your suspended account, you should write
+                            an email to <code>support@comicstack.com</code> to
+                            explain why we should get back your account and that
+                            you guarantee that you will never violate our{" "}
+                            <code>Terms of Use</code> again.
+                            <br />
+                            Do not forget to write your BAN code in the subject
+                            of the email(visit our support page for details).
                         </AccordionPanel>
                     </AccordionItem>
 
@@ -181,12 +220,10 @@ const FAQ = () => {
                             </AccordionButton>
                         </h2>
                         <AccordionPanel textAlign="left">
-                            You can support us via footer section button
-                            <code id="coffee">{" "}Buy Us Lots of Coffee☕</code>
-                            .
+                            You can support us via footer section button called{" "}
+                            <code id="coffee">Buy Us Lots of Coffee☕</code>.
                         </AccordionPanel>
                     </AccordionItem>
-
                 </Accordion>
             </div>
         </ChakraProvider>
