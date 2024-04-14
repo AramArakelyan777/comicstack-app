19b905d11dacf92ec4a4eb9f0117581e4e60b933
Aram Arakelyan <aram777arakelyan@gmail.com>
2024-04-14 14:53:25 +0400

    added subscribe form

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,8 +1,72 @@
 import React from "react"
 import "./Footer.css"
+import "../../assets/texts.css"
+import logo from "../../assets/images/footer/DarkLogo.png"
+import Input from "../Input/Input"
+import Button from "../Button/Button"
+import { useFormik } from "formik"
+import * as yup from "yup"
+
+const onSubmit = (values, onSubmitProps) => {
+    console.log("Form values", values)
+    onSubmitProps.resetForm()
+}
+
+const validationSchema = yup.object().shape({
+    subscribeEmail: yup
+        .string()
+        .required("Required")
+        .matches(/(?=.{6,50}$)/, "Email must be between 6 and 50 characters")
+        .email("Invalid email address"),
+})
 
 function Footer() {
-    return <div>Footer</div>
+    const formik = useFormik({
+        initialValues: { subscribeEmail: "" },
+        onSubmit,
+        validationSchema,
+        validateOnChange: true,
+        validateOnBlur: true,
+    })
+
+    return (
+        <div className="footer">
+            <img className="darkLogo" src={logo} alt="logo" />
+            <div className="footer-content">
+                <div>
+                    <p>Subscribe to get our newsletter!</p>
+                    <form onSubmit={formik.handleSubmit}>
+                        <div>
+                            <Input
+                                type="email"
+                                name="subscribeEmail"
+                                id="subscribeEmail"
+                                {...formik.getFieldProps("subscribeEmail")}
+                                variant="small"
+                                placeholder=""
+                            />
+                            {formik.touched.subscribeEmail &&
+                            formik.errors.subscribeEmail ? (
+                                <div className="error small-text">
+                                    {formik.errors.subscribeEmail}
+                                </div>
+                            ) : null}
+                        </div>
+                        <Button
+                            variant="ordinary"
+                            type="submit"
+                            id="subscribeEmail"
+                            disabled={!formik.isValid}
+                        >
+                            Subscribe
+                        </Button>
+                    </form>
+                    <p>Please help us keep ComicStack free!</p>
+                </div>
+                <div className="info"></div>
+            </div>
+        </div>
+    )
 }
 
 export default Footer
--- client/src/components/pages/Genres/Genres.js
+++ client/src/components/pages/Genres/Genres.js
@@ -2,24 +2,28 @@ import React from "react"
 import "../../../assets/texts.css"
 import { IMAGES } from "./images"
 import "./Genres.css"
+import Footer from "../../Footer/Footer"
 
 const Genres = () => {
     return (
-        <div className="genres">
-            <h1 className="bigger-heading">GENRES</h1>
-            <div>
-                {IMAGES.map((imageGroup) => (
-                    <div key={imageGroup[0].id} className="column">
-                        {imageGroup.map((image) => (
-                            <div className="container" key={image.id}>
-                                <img src={image.src} alt="genre" />
-                                <div className="centered">{image.text}</div>
-                            </div>
-                        ))}
-                    </div>
-                ))}
+        <React.Fragment>
+            <div className="genres">
+                <h1 className="bigger-heading">GENRES</h1>
+                <div>
+                    {IMAGES.map((imageGroup) => (
+                        <div key={imageGroup[0].id} className="column">
+                            {imageGroup.map((image) => (
+                                <div className="container" key={image.id}>
+                                    <img src={image.src} alt="genre" />
+                                    <div className="centered">{image.text}</div>
+                                </div>
+                            ))}
+                        </div>
+                    ))}
+                </div>
             </div>
-        </div>
+            <Footer />
+        </React.Fragment>
     )
 }
 
