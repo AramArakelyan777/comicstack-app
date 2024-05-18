71c284029bc006973660af254aaf31a77f66728e
Aram Arakelyan <aram777arakelyan@gmail.com>
2024-05-18 09:04:43 +0400

    added contexts for threads and comics

---
+++ client/src/context/ComicsContext.js
@@ -0,0 +1,99 @@
+import React, {
+    useContext,
+    createContext,
+    useMemo,
+    useState,
+    useEffect,
+} from "react"
+import { useAsync } from "../hooks/useAsync"
+import { getAComics } from "../services/comics"
+import { useParams } from "react-router-dom"
+
+const ComicsContext = createContext(null)
+
+export const handleRequestError = (error) => {
+    return error?.response?.data?.message ?? "Error"
+}
+
+export function useComics() {
+    return useContext(ComicsContext)
+}
+
+function ComicsContextProvider({ children }) {
+    const { comic_id } = useParams()
+
+    const {
+        loading,
+        error,
+        value: comic,
+    } = useAsync(() => getAComics(comic_id), [comic_id])
+
+    const [comments, setComments] = useState([])
+
+    const commentsByParentId = useMemo(() => {
+        if (!comments) return []
+        const group = {}
+
+        comments.forEach((comment) => {
+            group[comment?.parent_id] ||= []
+            group[comment?.parent_id].push(comment)
+        })
+
+        return group
+    }, [comments])
+
+    useEffect(() => {
+        if (comic?.comments == null) return
+        setComments(comic?.comments)
+    }, [comic?.comments])
+
+    function getReplies(parent_id) {
+        return commentsByParentId[parent_id]
+    }
+
+    function createLocalComment(comment) {
+        setComments((prevComments) => [comment, ...prevComments])
+    }
+
+    function updateLocalComment(comment_id, message) {
+        setComments((prevComments) => {
+            const updatedComments = prevComments.map((comment) => {
+                if (comment.comment_id === comment_id)
+                    return { ...comment, messages: message }
+                else return comment
+            })
+            return updatedComments
+        })
+    }
+
+    function deleteLocalComment(comment_id) {
+        setComments((prevComments) => {
+            return prevComments.filter(
+                (comment) => comment.comment_id !== comment_id
+            )
+        })
+    }
+
+    return (
+        <ComicsContext.Provider
+            value={{
+                comic: { comic_id, ...comic },
+                getReplies,
+                createLocalComment,
+                updateLocalComment,
+                deleteLocalComment,
+                rootComments: commentsByParentId[null],
+            }}
+        >
+            {loading ? (
+                <h1>Loading...</h1>
+            ) : error ? (
+                <h1>{error}</h1>
+            ) : (
+                children
+            )}
+        </ComicsContext.Provider>
+    )
+}
+
+export default ComicsContextProvider
---
+++ client/src/context/ThreadContext.js
@@ -0,0 +1,99 @@
+import React, {
+    createContext,
+    useContext,
+    useEffect,
+    useMemo,
+    useState,
+} from "react"
+import { useParams } from "react-router-dom"
+import { useAsync } from "../hooks/useAsync"
+import { getAThread } from "../services/threads"
+
+const ThreadContext = createContext(null)
+
+export const handleRequestError = (error) => {
+    return error?.response?.data?.message ?? "Error"
+}
+
+export function useThread() {
+    return useContext(ThreadContext)
+}
+
+function ThreadContextProvider({ children }) {
+    const { thread_id } = useParams()
+
+    const {
+        loading,
+        error,
+        value: thread,
+    } = useAsync(() => getAThread(thread_id), [thread_id])
+
+    const [comments, setComments] = useState([])
+
+    const commentsByParentId = useMemo(() => {
+        if (!comments) return []
+        const group = {}
+
+        comments.forEach((comment) => {
+            group[comment?.parent_id] ||= []
+            group[comment?.parent_id].push(comment)
+        })
+
+        return group
+    }, [comments])
+
+    useEffect(() => {
+        if (thread?.comments == null) return
+        setComments(thread?.comments)
+    }, [thread?.comments])
+
+    function getReplies(parent_id) {
+        return commentsByParentId[parent_id]
+    }
+
+    function createLocalComment(comment) {
+        setComments((prevComments) => [comment, ...prevComments])
+    }
+
+    function updateLocalComment(comment_id, message) {
+        setComments((prevComments) => {
+            const updatedComments = prevComments.map((comment) => {
+                if (comment.comment_id === comment_id)
+                    return { ...comment, messages: message }
+                else return comment
+            })
+            return updatedComments
+        })
+    }
+
+    function deleteLocalComment(comment_id) {
+        setComments((prevComments) => {
+            return prevComments.filter(
+                (comment) => comment.comment_id !== comment_id
+            )
+        })
+    }
+
+    return (
+        <ThreadContext.Provider
+            value={{
+                thread: { thread_id, ...thread },
+                getReplies,
+                createLocalComment,
+                updateLocalComment,
+                deleteLocalComment,
+                rootComments: commentsByParentId[null],
+            }}
+        >
+            {loading ? (
+                <h1>Loading...</h1>
+            ) : error ? (
+                <h1>{error}</h1>
+            ) : (
+                children
+            )}
+        </ThreadContext.Provider>
+    )
+}
+
+export default ThreadContextProvider
